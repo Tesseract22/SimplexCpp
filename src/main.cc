3fabb0b18bced47e5f362ce5731df47e003ff41d
Tesseract22 <caleb2222@qq.com>
2023-04-13 14:07:18 -0500

    update

--- src/main.cc
+++ src/main.cc
@@ -6,15 +6,14 @@
 #include <array>
 using namespace std;
 int main() {
-    const int M = 3; const int N = 2; // 3 constraints and 2 variables
+    const int M = 2; const int N = 2; // 3 constraints and 2 variables
     float ineq_lhs[M][N] = {
-        {1, 2},
         {1, 1},
-        {3, 2},
+        {2, 1},
     };
-    array<float, M> ineq_rhs = {16, 9, 24};
+    array<float, M> ineq_rhs = {12, 16};
     array<float, N> obj = {40, 30};
-    Matrix<float, 3, 2> l(ineq_lhs);
+    Matrix<float, M, N> l(ineq_lhs);
     // auto t = ToTableau<M, N>(obj, l, ineq_rhs);
     // // t.rowAddition(0, 1);
     // t.debugPrint();
